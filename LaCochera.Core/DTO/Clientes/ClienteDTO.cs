b3d6f7d59a1ded27649199d402fb3a71534bc980
Jesús Cañas Cobos <jesus.c.cobos@gmail.com>
2021-02-17 16:16:38 +0100

    Se generan más equals en los DTO

--- LaCochera.Core/DTO/Clientes/ClienteDTO.cs
+++ LaCochera.Core/DTO/Clientes/ClienteDTO.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace LaCochera.Core.DTO.Clientes
 {
-    public class ClienteDTO
+    public class ClienteDTO : IEquatable<ClienteDTO>
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
@@ -15,5 +15,34 @@ namespace LaCochera.Core.DTO.Clientes
         public int Presupuesto { get; set; }
         public DateTime FechaRegistro { get; set; }
         public string DescripcionVehiculo { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ClienteDTO);
+        }
+
+        public bool Equals(ClienteDTO other)
+        {
+            return other != null &&
+                   Id == other.Id &&
+                   Nombre == other.Nombre &&
+                   Apellidos == other.Apellidos &&
+                   Telefono == other.Telefono &&
+                   Dni == other.Dni &&
+                   Email == other.Email &&
+                   Presupuesto == other.Presupuesto &&
+                   FechaRegistro == other.FechaRegistro &&
+                   DescripcionVehiculo == other.DescripcionVehiculo;
+        }
+
+        public static bool operator ==(ClienteDTO left, ClienteDTO right)
+        {
+            return EqualityComparer<ClienteDTO>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(ClienteDTO left, ClienteDTO right)
+        {
+            return !(left == right);
+        }
     }
 }
--- LaCochera.Core/DTO/Ventas/PropuestaVentaAmpliadoDTO.cs
+++ LaCochera.Core/DTO/Ventas/PropuestaVentaAmpliadoDTO.cs
@@ -1,16 +1,39 @@
 ﻿using LaCochera.Core.DTO.Clientes;
 using LaCochera.Core.DTO.Vehiculos;
 using LaCochera.Core.DTO.Ventas;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace LaCochera.Core.DTO
 {
-    public class PropuestaVentaAmpliadoDTO : Ventas.PropuestaVentaDTO
+    public class PropuestaVentaAmpliadoDTO : PropuestaVentaDTO
     {
         public ClienteDTO Cliente { get; set; }
         public VehiculoVenderDTO VehiculoVender { get; set; }
         public VendedorDTO Vendedor { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is PropuestaVentaAmpliadoDTO dTO &&
+                   base.Equals(obj) &&
+                   Id == dTO.Id &&
+                   Estado == dTO.Estado &&
+                   FechaInicio == dTO.FechaInicio &&
+                   FechaFin == dTO.FechaFin &&
+                   FechaLimite == dTO.FechaLimite &&
+                   Presupuesto == dTO.Presupuesto &&
+                   EqualityComparer<ClienteDTO>.Default.Equals(Cliente, dTO.Cliente) &&
+                   EqualityComparer<VehiculoVenderDTO>.Default.Equals(VehiculoVender, dTO.VehiculoVender) &&
+                   EqualityComparer<VendedorDTO>.Default.Equals(Vendedor, dTO.Vendedor);
+        }
+
+        public static bool operator ==(PropuestaVentaAmpliadoDTO left, PropuestaVentaAmpliadoDTO right)
+        {
+            return EqualityComparer<PropuestaVentaAmpliadoDTO>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(PropuestaVentaAmpliadoDTO left, PropuestaVentaAmpliadoDTO right)
+        {
+            return !(left == right);
+        }
     }
 }
